1794d356bbee32bc556f6addbabb1c0d79fb8434
tracer-nju <tracer@nju.edu.cn>
2020-02-20 21:02:42 +0800

    Static hostname: zhx-book          Icon name: computer-laptop            Chassis: laptop         Machine ID: 861886194ba44b1eb38fb6b009209bbe            Boot ID: 2367304b98034521854c97c6a64873a9   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 4.15.0-76-generic       Architecture: x86-64  21:02:42 up  7:21,  1 user,  load average: 5.90, 5.02, 4.52

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -9,8 +9,9 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0) show_pid = 1;
-    if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0) num_sort = 1;
-    if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) show_version = 1;
+    else if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0) num_sort = 1;
+    else if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) show_version = 1;
+    else {printf("illegal args\n"); assert(0);}
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   printf("p: %d, n: %d, V: %d\n", show_pid, num_sort, show_version);
