49db5e44c9ef97c9a3b5ce8debb4d2d6e21138c6
tracer-nju <tracer@nju.edu.cn>
2020-02-20 22:27:35 +0800

    Static hostname: zhx-book          Icon name: computer-laptop            Chassis: laptop         Machine ID: 861886194ba44b1eb38fb6b009209bbe            Boot ID: 2367304b98034521854c97c6a64873a9   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 4.15.0-76-generic       Architecture: x86-64  22:27:35 up  8:45,  1 user,  load average: 1.81, 1.64, 1.81

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,19 +2,30 @@
 #include <string.h>
 #include <assert.h>
 
+extern void show_version();
+
 int main(int argc, char *argv[]) {
-  int show_pid = 0;
-  int num_sort = 0;
-  int show_version = 0;
+  int show_p = 0;
+  int num_s = 0;
+  int show_v = 0;
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
-    if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0) show_pid = 1;
-    else if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0) num_sort = 1;
-    else if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) show_version = 1;
-    else {printf("illegal args\n"); assert(0);}
+    if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0) show_p = 1;
+    if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0) num_s = 1;
+    if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) show_v = 1;
     printf("argv[%d] = %s\n", i, argv[i]);
   }
-  printf("p: %d, n: %d, V: %d\n", show_pid, num_sort, show_version);
+  printf("p: %d, n: %d, V: %d\n", show_p, num_s, show_v);
   assert(!argv[argc]);
+  if (show_v == 1){
+    show_version();
+    return 0;
+  }
   return 0;
+}
+
+void show_version(){
+  printf("pstree (designed by Hanxiao Zhang) 1.0\n Copyright (C) 2020 Hanxiao Zhang (a vegetable bird)\n");
+  printf("\nThis is a poor and free software\nAnd if anything goes wrong, don't ask me.");
+  return;
 }
