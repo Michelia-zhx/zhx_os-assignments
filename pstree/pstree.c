0cf3af7277d18cfde41e82eb72d601e1cc68ce0b
tracer-nju <tracer@nju.edu.cn>
2020-02-20 20:48:38 +0800

    Static hostname: zhx-book          Icon name: computer-laptop            Chassis: laptop         Machine ID: 861886194ba44b1eb38fb6b009209bbe            Boot ID: 2367304b98034521854c97c6a64873a9   Operating System: Ubuntu 18.04.3 LTS             Kernel: Linux 4.15.0-76-generic       Architecture: x86-64  20:48:38 up  7:07,  1 user,  load average: 5.86, 4.18, 4.27

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,10 +2,17 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
+  int show_pid = 0;
+  int num_sort = 0;
+  int show_version = 0;
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
+    if (argv[i] == "-p" || argv[i] == "--show-pids") show_pid = 1;
+    if (argv[i] == "-n" || argv[i] == "--numeric-sort") num_sort = 1;
+    if (argv[i] == "-V" || argv[i] == "--version") show_version = 1;
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+  printf("p: %d, n: %d, V: %d\n", show_pid, num_sort, show_version);
   assert(!argv[argc]);
   return 0;
 }
